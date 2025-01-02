4021a5dc2a16ef551aad3d17117b5fe40fe87cff
Krishna8-run <1sahkrishna8@gmail.com>
2025-01-02 11:39:45 +0545

    y

--- lower case to upper case.cpp
+++ lower case to upper case.cpp
@@ -3,8 +3,9 @@ int main (){
 	char c;
 	printf("enter the num in upper case\n");
 	scanf("%c",&c);
-	if(c>65&&c<90){
+	if(c>=65&&c<=90){
 		c=c+32;
 		 printf("lowercase is %c",c);
 	}
+	return 0;
 }
